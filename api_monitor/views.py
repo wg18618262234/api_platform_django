1767949584d37ee5f5955ecd7afd51ff26c24971
wanggang_test <wanggang_test@guanghe.tv>
2020-08-27 00:50:15 +0800

    update

--- api_monitor/views.py
+++ api_monitor/views.py
@@ -1,44 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_GET, require_POST
 # Create your views here.
 
 
-@require_http_methods(["POST"])
+@require_POST
 def api_add(request):
     return JsonResponse({"hello": "worlds"})
 
 
-@require_http_methods(["POST"])
+@require_POST
 def api_update(request):
     return JsonResponse({"hello": "worlds"})
 
 
-@require_http_methods(["POST"])
+@require_POST
 def api_delete(request):
     return JsonResponse({"hello": "worlds"})
 
 
-@require_http_methods(["GET"])
+@require_GET
 def api_get_list(request):
     return JsonResponse({"hello": "worlds"})
 
 
-@require_http_methods(["POST"])
+@require_POST
 def monitor_add(request):
     return JsonResponse({"hello": "worlds"})
 
 
-@require_http_methods(["POST"])
+@require_POST
 def monitor_update(request):
     return JsonResponse({"hello": "worlds"})
 
 
-@require_http_methods(["POST"])
+@require_POST
 def monitor_delete(request):
     return JsonResponse({"hello": "worlds"})
 
 
-@require_http_methods(["GET"])
+@require_GET
 def monitor_get_list(request):
     return JsonResponse({"hello": "worlds"})
