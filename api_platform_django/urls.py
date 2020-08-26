8b42f4c9a1845b67476aab1f67333f0b11f657f3
wanggang_test <wanggang_test@guanghe.tv>
2020-08-27 00:42:45 +0800

    update

--- api_platform_django/urls.py
+++ api_platform_django/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api_monitor.urls')),
+    path('api/apiMonitor/', include('api_monitor.urls')),
 ]
