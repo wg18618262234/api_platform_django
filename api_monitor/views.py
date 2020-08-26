2634b82f49ea1f7ec40d8d0967d324b1ccb234fa
wanggang_test <wanggang_test@guanghe.tv>
2020-08-27 00:35:38 +0800

    update

--- api_monitor/views.py
+++ api_monitor/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
+
 def api_add(request):
-    return JsonResponse({"hello":"worlds"})
+    return JsonResponse({"hello": "worlds"})
+
+
+def api_update(request):
+    return JsonResponse({"hello": "worlds"})
+
+
+def api_delete(request):
+    return JsonResponse({"hello": "worlds"})
+
+
+def api_get_list(request):
+    return JsonResponse({"hello": "worlds"})
+
+
+def monitor_add(request):
+    return JsonResponse({"hello": "worlds"})
+
+
+def monitor_update(request):
+    return JsonResponse({"hello": "worlds"})
+
+
+def monitor_delete(request):
+    return JsonResponse({"hello": "worlds"})
+
+
+def monitor_get_list(request):
+    return JsonResponse({"hello": "worlds"})
